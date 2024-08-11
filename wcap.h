5c462d5619043bdff8858e2622a2ba67c4676c98
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-08-11 16:07:50 -0700

    Disable known warnings in 'wcap.h'
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- wcap.h
+++ wcap.h
@@ -3,11 +3,23 @@
 #define UNICODE
 #define COBJMACROS
 #define WIN32_LEAN_AND_MEAN
+
+// ReSharper disable CppClangTidyClangDiagnosticReservedMacroIdentifier
 #define _CRT_SECURE_NO_DEPRECATE
 
+// push warning
+#pragma warning(push)
+#pragma warning(disable: 4255)
+#pragma warning(disable: 4456)
+#pragma warning(disable: 4668)
+#pragma warning(disable: 4459)
+#pragma warning(disable: 4777)
+#pragma warning(disable: 4459)
+#pragma warning(disable: 4820)
 #include <initguid.h>
 #include <windows.h>
 #include <intrin.h>
+#pragma warning(pop)
 
 #define WCAP_TITLE L"wcap"
 #define WCAP_URL   L"https://github.com/mmozeiko/wcap"
@@ -27,3 +39,21 @@
 
 #include <stdio.h>
 #define StrFormat(Buffer, ...) _snwprintf(Buffer, _countof(Buffer), __VA_ARGS__)
+
+#pragma warning(disable: 4211)
+#pragma warning(disable: 4255)
+#pragma warning(disable: 4668)
+#pragma warning(disable: 4777)
+#pragma warning(disable: 4820)
+#pragma warning(disable: 4456)
+#pragma warning(disable: 4710)
+#pragma warning(disable: 4711)
+#pragma warning(disable: 5045)
+#pragma warning(disable: 4057)
+#pragma warning(disable: 4100)
+#pragma warning(disable: 4189)
+#pragma warning(disable: 4242)
+#pragma warning(disable: 4244)
+#pragma warning(disable: 4389)
+#pragma warning(disable: 4514)
+#pragma warning(disable: 4245)
